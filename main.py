494202af054c78e1514620e0ffcf76cb88b3bd47
ravinthiranpartheepan1407 <ravinthiranpartheepan1407@gmail.com>
2024-07-30 21:59:04 +0300

    fix

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from rag import ChatPDF
 __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+import sqlite3
 
 st.set_page_config(
     page_title="Chat Psychologist AI Sandbox"
