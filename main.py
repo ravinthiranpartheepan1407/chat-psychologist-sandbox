6e9f741b2d08fafc63a1025cca6aa4566c89f7af
ravinthiranpartheepan1407 <ravinthiranpartheepan1407@gmail.com>
2024-07-30 23:57:55 +0300

    IP Map

--- main.py
+++ main.py
@@ -27,7 +27,18 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
-st.markdown(hide_github_icon, unsafe_allow_html=True)
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 def display_messages():
     st.subheader("RAG-NET Sandbox")
